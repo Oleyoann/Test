18fda572109608f92b443c78374adc1e534e11ca
Oleyoann <whateva@gmx.fr>
2015-06-23 16:00:36 -0700

    Changes Jun 23rd

--- whateva/js/app.js
+++ whateva/js/app.js
@@ -56,9 +56,9 @@ window.yh = (function(){
         }, false);
     };
 
-    var setCellHeight = function(){
+    var setCellHeight = function() {
 
-        var pageWidth = window.outerWidth,
+        var pageWidth = screen.width,
             cellHeight = Math.round(pageWidth / 2);
 
         s.innerHTML = '#list li div { height:' + cellHeight + 'px;}'
@@ -82,10 +82,6 @@ window.yh = (function(){
     };
 
     init();
-
-    return {
-
-    }
 
 }());
 
